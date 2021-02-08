c3ff0ab1d0d5401b3c08399da655239684938413
Kris Sankaran <sankaran.kris@gmail.com>
2021-02-08 22:53:51 +0000

    function to save preds

--- mappingmodel/train.py
+++ mappingmodel/train.py
@@ -5,6 +5,7 @@ Training/Validation Module
 from pathlib import Path
 import argparse
 import os
+import re
 import numpy as np
 import torch
 import torch.nn.functional as F
@@ -18,12 +19,32 @@ def l2_reg(params, device):
 
 def loss(y_hat, y, params, device, smooth=0.2, weights=[0.6, 0.9, 0.2], lambda_reg=0.0005):
     penalty = l2_reg(params, device)
-    return dice_bce_loss(y_hat, y, device, smooth, weights) + lambda_reg * penalty
+    dice = dice_loss(y_hat, y, device, weights, smooth)
+    bce = bce_loss(y_hat, y, device, weights)
+    return dice + bce + lambda_reg * penalty
+
+
+def dice_loss(y_hat, y, device, weights, smooth):
+    dims = (0,) + tuple(range(2, y.ndimension()))
+    intersection = torch.sum(y_hat * y, dims)
+    union = torch.sum(y_hat + y, dims)
+    dice = 1 - (2. * intersection / (union + smooth))
+    weights = torch.Tensor(weights).to(device)
+    return (weights * dice).mean()
+
+
+def bce_loss(y_hat, y, device, weights):
+    w_mat = torch.ones_like(y).to(device)
+    for k in range(y.shape[1]):
+        w_mat[:, k, :, :] = weights[k]
+    return F.binary_cross_entropy(y_hat, y, weight=w_mat, reduction="mean")
 
 
 def dice_bce_loss(y_hat, y, device, smooth=0.2, weights=[0.6, 0.9, 0.2]):
     y_hat = y_hat.view(-1)
     y = y.view(-1)
+    import pdb
+    pdb.set_trace()
 
     intersection = (y_hat * y).sum()
     dice_loss = 1 - (2. * intersection + smooth)/(y_hat.sum() + y.sum() + smooth)
@@ -72,3 +93,17 @@ def log_batch(epoch, i, n, loss, batch_size):
         flush=True
     )
 
+
+def predictions(model, ds, out_dir, device):
+    create_dir(out_dir)
+    
+    for i in range(len(ds)):
+        x, y = ds[i]
+        ix = re.search("[0-9]+", str(ds.x_paths[i])).group(0)
+        print(f"saving {i + 1}/{len(ds)}...", end="\r", flush=True)
+
+        with torch.no_grad():
+            y_hat = model(x.unsqueeze(0).to(device))
+            np.save(out_dir / f"y_hat-{i}.npy", y_hat.cpu()[0])
+            np.save(out_dir / f"y-{i}.npy", y)
+            np.save(out_dir / f"x-{i}.npy", x)
