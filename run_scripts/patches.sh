5d3748d5550348729cae6b1192d4fb46395a3360
Kris Sankaran <sankaran.kris@gmail.com>
2021-05-06 22:04:09 -0500

    update the train submit script

--- run_scripts/patches.sh
+++ run_scripts/patches.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 git clone https://github.com/krisrs1128/mappingvis.git
-((B++))
-Rscript -e "rmarkdown::render('mappingvis/mappingvis/2-preprocess.Rmd', params = list(n_patches=10))"
-tar -zcvf patches_${B}.tar.gz processed
+Rscript -e "rmarkdown::render('mappingvis/mappingvis/2-preprocess.Rmd')"
+tar -zcvf processed_${B}.tar.gz mappingvis/mappingvis/processed
 mv processed_${B}.tar.gz $_CONDOR_SCRATCH_DIR/
